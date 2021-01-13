cd34093ec4a4b0f0517fb1d658444e8034758466
handsomemolly <ryan.kendig@gmail.com>
2021-01-12 16:09:54 -0800

    commit tree b92fc74b9f30e35d7b26eac4cd0ea1fa252dccef

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // console.log("The DOM has loaded");
+    updateText()
+});
+
+function updateText() {
+let p = document.getElementById("text")
+p.textContent = "This is really cool!";
+}
